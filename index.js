9e68c31dad723946a99ae605d5fd4037377c303f
Adunnii <101995896+Adunnii@users.noreply.github.com>
2022-06-03 16:51:50 +0100

    making seconds count

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const today = new Date();
-const birthday = 6
+const birthday = new Date('June 4, 2022 12:00:00').getTime();
 
-document.getElementById("box_1").innerText = birthday - today.getDay()
-document.getElementById("box-2").innerText =today.getHours()
-document.getElementById("box-3").innerText=today.getMinutes()
-document.getElementById("box-4").innerText=today.getSeconds()
+console.log(birthday)
+
+const countDown = setInterval(()=>{
+    const today = new Date().getTime();
+    
+    const distanceBetweendays =  birthday - today;
+
+    let days = Math.floor(distanceBetweendays / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distanceBetweendays % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distanceBetweendays % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distanceBetweendays % (1000 * 60)) / 1000);
+    
+    document.getElementById("box_1").innerHTML = days
+    document.getElementById("box-2").innerHTML =hours
+    document.getElementById("box-3").innerHTML=minutes
+    document.getElementById("box-4").innerHTML= seconds
+
+
+    if (distanceBetweendays < 0) {
+        clearInterval(x);
+        document.getElementById("main_text").innerHTML = "Happy Birthday OYIN!!";
+      }
+
+}, 1000) 
+
+
+console.log(countDown)
+
